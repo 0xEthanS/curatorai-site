a9465552fdeed5201283c721f03b66729bc4421f
Ethan <0xethans@gmail.com>
2025-07-29 10:38:54 -0400

    Clean slate

--- curatorai-site/data/initialMessage.ts
+++
@@ -1,8 +0,0 @@
-export const initalMessage = [
-    { 
-        id: 1,
-        content: "Hi 👋 Ask me anything about the dos! Also: I can make you a personalized postcards if you give me your name." 
-    }
-] 
-
-
